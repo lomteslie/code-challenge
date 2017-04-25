8703505cc86d95a2164d622ac6f1e9404fb4f71b
Tom Leslie <lomteslie@gmail.com>
2017-04-25 09:15:51 -0500

    Create Hamming class

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -2,6 +2,22 @@
 // `npm test` to ensure the unit tests pass and
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
+class Hamming {
+  compute (strandA = '', strandB = '') {
+    if (strandA.length !== strandB.length) {
+      throw new Error('DNA strands must be of equal length.')
+    }
 
+    const strandBArray = strandB.split('')
+
+    return strandA.split('').reduce((prev, value, index) => {
+      if (value !== strandBArray[index]) {
+        return prev + 1
+      }
+
+      return prev
+    }, 0)
+  }
+}
 
 module.exports = Hamming;
